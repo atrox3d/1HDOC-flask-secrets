89b1b78bf22ee6b7c64e8fb713ac385cf3f3d96d
atrox3d <atrox3d@gmail.com>
2021-04-07 19:06:29 +0200

    create form

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
 import util.logging
+from util.logging import log_decorator
 import util.network
 import util.params
 import logging
 
 logger = logging.getLogger(__name__)
-
-
+root = util.logging.get_root_logger()
 app = Flask(__name__)
+app.secret_key = "what-the-fuck"
 
 
 @app.route("/")
+@log_decorator
 def home():
     return render_template('index.html')
 
 
+class MyForm(FlaskForm):
+    name = StringField('email')
+    password = StringField('password')
+
+
+@app.route("/login")
+@log_decorator
+def login():
+    form = MyForm()
+    return render_template('login.html', form=form)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host=util.network.get_ipaddress())
-
