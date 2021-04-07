746219c2cd51033533400eb479e3d8dae829b14e
atrox3d <atrox3d@gmail.com>
2021-04-07 17:10:23 +0200

    add logging and utils

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from flask import Flask, render_template
+import util.logging
+import util.network
+import util.params
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 app = Flask(__name__)
 
@@ -9,4 +16,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=util.network.get_ipaddress())
+
